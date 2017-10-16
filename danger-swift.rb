ab905cfb972df37856bd3f4af3aee34ec55421ef
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-16 09:56:56 -0700

    Clean up formula (derp)

--- danger-swift.rb
+++ danger-swift.rb
@@ -4,17 +4,11 @@ class SourceDocs < Formula
   version "0.1.0"
   url "https://github.com/danger/danger-swift/archive/#{version}.tar.gz"
   sha256 "a9538c7d25ce1a58a01c0562a9902257e53e4bb396a2ab33ee69fb29e76845c4"
-  head "https://github.com/eneko/SourceDocs.git"
+  head "https://github.com/danger/danger-swift.git"
 
   depends_on :xcode
 
-  # def install
-  #   system "make", "install", "PREFIX=#{prefix}"
-  # end
   def install
-    build_path = "#{buildpath}/.build/release/sourcedocs"
-    ohai "Building SourceDocs"
-    system("swift --disable-sandbox build -c release -Xswiftc -static-stdlib")
-    bin.install build_path
+    system "make", "install"
   end
 end

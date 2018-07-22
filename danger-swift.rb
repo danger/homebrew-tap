82936755c6bf9be84af99542e424020dc61c582f
Orta <orta.therox+github@gmail.com>
2018-07-22 07:13:37 -0400

    Update danger-swift.rb

--- danger-swift.rb
+++ danger-swift.rb
@@ -1,9 +1,9 @@
 class DangerSwift < Formula
   desc "Write your Dangerfiles in Swift"
   homepage "https://github.com/danger/danger-swift"
-  version "0.4.0"
+  version "0.4.1"
   url "https://github.com/danger/danger-swift/archive/#{version}.tar.gz"
-  sha256 "7d924b866884cb92074bf1678fe967701731fafab8921a35083cb590b9d9722d"
+  sha256 "4c181f65492cb43879b706e39f52abe23013c14ffa6dba130ec4344c240db9c2"
   head "https://github.com/danger/danger-swift.git"
 
   depends_on :xcode

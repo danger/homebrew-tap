860d38f0ee4dd7cad0444ad83408a21d60fc011d
Orta Therox <orta.therox@gmail.com>
2018-11-10 09:04:53 -0500

    Releasing danger-js version 6.0.6

--- danger-js.rb
+++ danger-js.rb
@@ -1,7 +1,7 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/6.0.5/danger-macos.zip"
-  sha256 "ec00d5b535781f16e9275681f547f5a0ea1fbc1348a4e8185c20192fc931b3f8"
+  url "https://github.com/danger/danger-js/releases/download/6.0.6/danger-macos.zip"
+  sha256 "b38a3c00cc8194a40d1669e35876362edaa9fa531436664a027db98b4868d816"
 
   def install
     bin.install "danger"

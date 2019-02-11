7136ffa2b443ebf0bedb0f5b0ab111fa0e7c206b
Franco Meloni <franco.meloni91@gmail.com>
2019-02-11 19:45:37 +0000

    Releasing danger-swift version 1.3.2

--- danger-swift.rb
+++ danger-swift.rb
@@ -1,9 +1,9 @@
 class DangerSwift < Formula
   desc "Write your Dangerfiles in Swift"
   homepage "https://github.com/danger/danger-swift"
-  version "1.3.1"
+  version "1.3.2"
   url "https://github.com/danger/danger-swift/archive/#{version}.tar.gz"
-  sha256 "66555ea7da240db5d71a90cc9a7947f42261fbc6c0251b7c350bf35366a51ff2"
+  sha256 "0fa8676643433ea0df7bafdaedf8024d41fde4fa270643da17e206071e6296e7"
   head "https://github.com/danger/danger-swift.git"
 
   # Runs only on Xcode 10

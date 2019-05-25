386ac16554177581de38a8d727f9bfe9382f6189
Franco Meloni <franco.meloni91@gmail.com>
2019-05-25 12:28:47 +0100

    Update danger-kotlin.rb

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -1,9 +1,9 @@
 class DangerKotlin < Formula
   desc "Write your Dangerfiles in Kotlin"
   homepage "https://github.com/danger/kotlin"
-  version "0.1.3"
+  version "0.1.5"
   url "https://github.com/danger/kotlin/archive/#{version}.tar.gz"
-  sha256 "aa75958b80967e3c411d70f222546d5bc646e349a8bd961c19e6432e2201f937"
+  sha256 "40d326174c307060e2ae21d4d386e64f3348f2399de7693e936634b4a494dd04"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger
@@ -14,6 +14,7 @@ class DangerKotlin < Formula
   depends_on "holgerbrandl/tap/kscript"
 
   def install
+    system "scripts/brew_install.sh"
     system "make", "brew", "PREFIX=#{prefix}"
   end
 end

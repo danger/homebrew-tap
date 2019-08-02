2aa7912fc0a0a2a22e1a4940a183af219afb33a8
Franco Meloni <franco.meloni91@gmail.com>
2019-08-02 23:04:52 +0100

    Update danger-js.rb

--- danger-js.rb
+++ danger-js.rb
@@ -1,7 +1,7 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/7.1.0/danger-macos.zip"
-  sha256 "c85f18cf9cfd3e139123deb66fd46a719df1f4a177abb0397daebf2d1afa132f"
+  url "https://github.com/danger/danger-js/releases/download/9.1.5/danger-macos.zip"
+  sha256 "33a4a48875070291348333dcc477ef653cec4c35ea9ec09e17f9d22ac767d8a2"
 
   def install
     bin.install "danger"

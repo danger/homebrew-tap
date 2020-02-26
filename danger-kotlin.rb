35d1f85fd771c9c85612cd7fb3cb68d295773e27
Franco Meloni <franco.meloni91@gmail.com>
2020-02-26 23:41:31 +0000

    Update danger-kotlin.rb

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -3,7 +3,7 @@ class DangerKotlin < Formula
   homepage "https://github.com/danger/kotlin"
   version "0.3.0"
   url "https://github.com/danger/kotlin/archive/#{version}.tar.gz"
-  sha256 "038c854bce6428cd5d785ad379226023ef558411c834a989e92f56bf975772e1"
+  sha256 "a846674dfc9f74e152dcb4045ecc0ea043c0ad7381ac546a9525487941cb7c83"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger

d5069eed525ec3a608845848b60d7fbd33dc5b44
Orta <git@orta.io>
2020-07-24 15:45:19 -0400

    Releasing danger-js version 10.3.0

--- danger-js.rb
+++ danger-js.rb
@@ -1,6 +1,6 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/10.2.1/danger-macos.zip"
+  url "https://github.com/danger/danger-js/releases/download/10.3.0/danger-macos.zip"
   sha256 "8c1d6c63cbcad391b366b0e632a7cb875113609b039b5a20d3d0e4bbabcfe6c8"
 
   def install

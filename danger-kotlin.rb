fb2fcb74db8a854e9b4d382ca932b7a1b03928ad
Gianluca Zuddas <29659183+gianluz@users.noreply.github.com>
2020-07-31 13:19:39 +0100

    Update danger-kotlin.rb

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -1,9 +1,9 @@
 class DangerKotlin < Formula
   desc "Write your Dangerfiles in Kotlin"
   homepage "https://github.com/danger/kotlin"
-  version "0.6.0"
+  version "0.6.1"
   url "https://github.com/danger/kotlin/archive/#{version}.tar.gz"
-  sha256 "dc3fbe20cd1a26bcd10f5f4e1fb8293a70eccc301b0e046fa6e5c9c3827c5a0a"
+  sha256 "088d15999e0a7d2bcab5a77634621654b9193e08cc0a98897d14d5a6d1b1f837"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger

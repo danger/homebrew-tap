767b8bc097cb21b6de8696e35e5d8b7230551785
Franco Meloni <franco.meloni91@gmail.com>
2021-08-04 13:56:33 +0200

    Releasing danger-swift version 3.10.2

--- danger-swift.rb
+++ danger-swift.rb
@@ -1,13 +1,13 @@
 class DangerSwift < Formula
   desc "Write your Dangerfiles in Swift"
   homepage "https://github.com/danger/danger-swift"
-  version "3.10.1"
+  version "3.10.2"
   url "https://github.com/danger/danger-swift/archive/#{version}.tar.gz"
-  sha256 "f7f12f3fc86287ea35185d6a738b76c93bcc11781a50880380159ba2061ec48b"
+  sha256 "a9919ab083846dd09297f2960522d69c48b8a2b00cca9b4003049e12f5c38c31"
   head "https://github.com/danger/danger-swift.git"
 
-  # Runs only on Xcode 11.4
-  depends_on :xcode => ["11.4", :build]
+  # Runs only on Xcode 10
+  depends_on :xcode => ["10", :build]
   # Use the vendored danger
   depends_on "danger/tap/danger-js"
 

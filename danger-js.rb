cd546d83ed41a9324be1100f0c978108c0924290
Orta Therox <git@orta.io>
2022-08-22 10:56:15 +0100

    Releasing danger-js version 11.1.2

--- danger-js.rb
+++ danger-js.rb
@@ -1,7 +1,7 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/11.1.1/danger-macos.zip"
-  sha256 "e975daff0ed90869fe35d8f3026c080dc007f3ec837fd8f7bcc409ec2b18f815"
+  url "https://github.com/danger/danger-js/releases/download/11.1.2/danger-macos.zip"
+  sha256 "5a814d758d3452a0bdd262a778b2fb9c5a81ed64553af6f210ab5edf52a41aa3"
 
   def install
     bin.install "danger"

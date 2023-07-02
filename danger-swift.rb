8a539f14b5a11fed96f25a1b188a5889b5ee5376
417-72KI <417.72ki@gmail.com>
2023-07-03 00:01:30 +0900

    restore danger-swift version

--- danger-swift.rb
+++ danger-swift.rb
@@ -1,7 +1,7 @@
 class DangerSwift < Formula
   desc "Write your Dangerfiles in Swift"
   homepage "https://github.com/danger/danger-swift"
-  version ""
+  version "3.17.0"
   url "https://github.com/danger/danger-swift/archive/#{version}.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/danger/danger-swift.git"

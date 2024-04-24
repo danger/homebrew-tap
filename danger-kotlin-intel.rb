e31a9fe825508d461a26721b7a17f393fef311df
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-04-24 19:33:21 +1000

    Update danger-kotlin-intel.rb

--- danger-kotlin-intel.rb
+++ danger-kotlin-intel.rb
@@ -3,7 +3,7 @@ class DangerKotlinIntel < Formula
   homepage "https://github.com/danger/kotlin"
   version "1.3.1"
   url "https://github.com/danger/kotlin/releases/download/#{version}/danger-kotlin-macosX64.tar"
-  sha256 "01ee5b770cefbcdf2aa0707aa7030132d762fc3d5422aa15fc4eab7110f6d4ef"
+  sha256 "df8bdab71d3590eec4a45375c6eb55c0fd801460893470a5b62782547089f4b0"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger

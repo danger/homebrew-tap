5dfa09a3816bf7e1d9a19cd1ef6b33718055b15b
Konstantin Aksenov <aksenov.kostya@gmail.com>
2025-11-12 14:01:35 +1000

    Update danger-kotlin to version 1.3.4

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -1,9 +1,9 @@
 class DangerKotlin < Formula
   desc "Write your Dangerfiles in Kotlin"
   homepage "https://github.com/danger/kotlin"
-  version "1.3.3"
+  version "1.3.4"
   url "https://github.com/danger/kotlin/releases/download/#{version}/danger-kotlin-macosArm64.tar"
-  sha256 "51eb28c29b50d83ea0054675fa8d1c8670ce1249761166fdc373349142983f1b"
+  sha256 "a6b6531da7a4c8274e41f54a075e85b4f4bc16e0a930a2259d938f4cdc1a24ea"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger
